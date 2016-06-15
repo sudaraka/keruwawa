ce385af9e0e08f4f966e517c1111af6825c55f6d
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-06-15 07:27:59 +0530

    Added copyright notice to where it's missing

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -7,6 +7,7 @@
  * This is free software, and you are welcome to redistribute it and/or modify
  * it under the terms of the BSD 2-clause License. See the LICENSE file for more
  * details.
+ *
  */
 
 /* eslint-disable no-process-env */
--- src/server/sql.js
+++ src/server/sql.js
@@ -1,3 +1,15 @@
+/**
+ * src/server/sql.js: common place for SQL statements used by the server
+ *
+ * Copyright 2016 Sudaraka Wijesinghe <sudaraka@sudaraka.org>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY
+ * This is free software, and you are welcome to redistribute it and/or modify
+ * it under the terms of the BSD 2-clause License. See the LICENSE file for more
+ * details.
+ *
+ */
+
 export const
 
   WEEKLY_ALL = `
