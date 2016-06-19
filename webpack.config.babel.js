822331610978288424ef67654490750d436df414
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-06-19 09:05:02 +0530

    webpack: extract shared configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,65 +1,55 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { join } from 'path'
+import merge from 'webpack-merge'
+
+const
+  sharedConfig = {
+    'output': {
+      'path': join(__dirname, 'dist'),
+      'filename': '[name]'
+    },
+
+    'module': {
+      'loaders': [
+        {
+          'test': /\.js$/,
+          'exclude': /node_modules/,
+          'loader': 'babel'
+        }
+      ]
+    }
+  }
 
 export default [
   // Server configuration
-  {
-    'target': 'node',
-    'context': join(__dirname, 'src/server'),
+  merge(sharedConfig,
+    {
+      'target': 'node',
+      'context': join(__dirname, 'src/server'),
 
-    'entry': { 'server.js': './cli.js' },
+      'entry': { 'server.js': './cli.js' },
 
-    'output': {
-      'path': join(__dirname, 'dist'),
-      'filename': '[name]'
-    },
+      'node': { '__dirname': false },
 
-    'node': { '__dirname': false },
-
-    'module': {
-      'loaders': [
-        {
-          'test': /\.js$/,
-          'exclude': /node_modules/,
-          'loader': 'babel'
-        }
-      ]
-    },
-
-    'externals': {
-      'deep-assign': 'commonjs deep-assign',
-      'express': 'commonjs express',
-      'mysql': 'commonjs mysql',
-      'yargs': 'commonjs yargs'
+      'externals': {
+        'deep-assign': 'commonjs deep-assign',
+        'express': 'commonjs express',
+        'mysql': 'commonjs mysql',
+        'yargs': 'commonjs yargs'
+      }
     }
-  },
+  ),
 
   // Client configuration
-  {
-    'context': join(__dirname, 'src/client'),
+  merge(sharedConfig,
+    {
+      'context': join(__dirname, 'src/client'),
 
-    'entry': { 'client.js': './index.js' },
+      'entry': { 'client.js': './index.js' },
 
-    'output': {
-      'path': join(__dirname, 'dist'),
-      'filename': '[name]'
-    },
-
-    'module': {
-      'loaders': [
-        {
-          'test': /\.js$/,
-          'exclude': /node_modules/,
-          'loader': 'babel'
-        }
+      'plugins': [
+        new HtmlWebpackPlugin({ 'template': join(__dirname, 'src/client/index.html') })
       ]
-    },
-
-    'plugins': [
-      new HtmlWebpackPlugin({
-        'excludeChunks': [ 'server.js' ],
-        'template': join(__dirname, 'src/client/index.html')
-      })
-    ]
-  }
+    }
+  )
 ]
