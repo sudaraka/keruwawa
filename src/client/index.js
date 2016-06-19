d16685d789365311e9e7dc89a5cf7486236d7557
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-06-19 20:20:26 +0530

    Render work hour data in a HTML table

--- src/client/index.js
+++ src/client/index.js
@@ -13,6 +13,20 @@
 import axios from 'axios'
 
 axios.get('/api/weekly-hours/').then(res => {
+  const
+    appDiv = document.querySelector('#app'),
+    tbl = document.createElement('table'),
+    thead = document.createElement('thead'),
+    tbody = document.createElement('tbody'),
+    trh = document.createElement('tr'),
+    weekList = Array(54).fill(0).map((_, i) => i),
+    data = res.data.payload
+
+  let
+    th
+
+  appDiv.innerHTML = null
+
   if(!res.data && 200 !== res.status) {
     console.error(res.statusText)
 
@@ -25,5 +39,47 @@ axios.get('/api/weekly-hours/').then(res => {
     return
   }
 
-  console.log(res.data.payload)
+  th = document.createElement('th')
+  th.textContent = 'Week'
+  trh.appendChild(th)
+
+  th = document.createElement('th')
+  th.textContent = 'Projects'
+  trh.appendChild(th)
+
+  th = document.createElement('th')
+  th.textContent = 'Hours'
+  trh.appendChild(th)
+
+  thead.appendChild(trh)
+
+  weekList
+    .forEach(week => {
+      const
+        tr = document.createElement('tr'),
+        key = `2016-${week}`
+
+      let
+        td
+
+      td = document.createElement('td')
+      td.textContent = week
+      tr.appendChild(td)
+
+      td = document.createElement('td')
+      td.textContent = Object.keys(data[key] || []).join(', ')
+      tr.appendChild(td)
+
+      td = document.createElement('td')
+      td.textContent = (Object.values(data[key] || []).reduce((a, v) => a + v, 0) / (60 * 60)).toFixed(2)
+      tr.appendChild(td)
+
+      tbody.appendChild(tr)
+    })
+
+  tbl.className = 'table'
+  tbl.appendChild(thead)
+  tbl.appendChild(tbody)
+
+  appDiv.appendChild(tbl)
 })
