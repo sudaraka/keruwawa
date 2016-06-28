00df7b4b10e7841a0df391f2daf1e2fa473f47d1
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-06-28 19:44:39 +0530

    Preparing production build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,11 +1,14 @@
 import webpack from 'webpack'
 import { join } from 'path'
 import merge from 'webpack-merge'
+import yargs from 'yargs'
 
 import serverConfig from './webpack-config/server'
 import clientConfig from './webpack-config/client'
 
 const
+  isProduction = yargs.argv.production,
+
   sharedConfig = {
     'output': {
       'path': join(__dirname, 'dist'),
@@ -26,12 +29,24 @@ const
       // Exclude moment.js locale from the build
       new webpack.IgnorePlugin(/locale/, /moment$/)
     ]
-  }
+  },
+
+  productionConfig = isProduction ? {
+    'plugins': [
+      new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify('production') } }),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        'minimize': true,
+        'compressor': { 'warnings': false }
+      })
+    ]
+  } : {}
 
 export default [
   // Server configuration
-  merge(sharedConfig, serverConfig),
+  merge(sharedConfig, productionConfig, serverConfig),
 
   // Client configuration
-  merge(sharedConfig, clientConfig)
+  merge(sharedConfig, productionConfig, clientConfig)
 ]
