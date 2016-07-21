1f0fd73802c6516bc69175f0686c40c4cddc7603
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-07-21 11:16:21 +0530

    Show version in the footer

--- webpack-config/client.js
+++ webpack-config/client.js
@@ -1,12 +1,17 @@
 import { join } from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
+import pkg from '../package'
+
 export default {
   'context': join(__dirname, '../src/client'),
 
   'entry': { 'client.js': './index.js' },
 
   'plugins': [
-    new HtmlWebpackPlugin({ 'template': join(__dirname, '../src/client/index.html') })
+    new HtmlWebpackPlugin({
+      'template': join(__dirname, '../src/client/index.html'),
+      'version': pkg.version
+    })
   ]
 }
